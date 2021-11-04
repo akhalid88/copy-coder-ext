72b72724492bef7636c0f7a462213024e9ecf9af
Muhammad K <akhalid.code@gmail.com>
2021-11-04 12:20:23 -0700

    updated README; create button variable and styling

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# first-extension
+# First-Extension
+
+This is my first extension. It will add a copy code button to code-blocks found on stackoverflows website
+
+-----------------------
+## Technologies
+* Javascript
+* Git/Github
+
+-----------------------
+## Contributing
+Clone repo.  
+In Chrome, go to ```chrome://extensions```  
+Enable Developer Mode  
+Click ```Load unpacked```  button and select extension directory.
+You must reload the extension, by clicking the reload button, aafter any changes made to the extension.
+
+-----------------------
+## Licenses
+This project uses a [MIT License](https://opensource.org/licenses/MIT). 
+
+-----------------------
+## Contact
+If you have any questions you can reach me at the following email: [akhalid.code@gmail.com](mailto:akhalid.code@gmail.com)
+
+Follow my other work at GitHub: https://github.com/akhalid88/
--- main.js
+++ main.js
@@ -1,11 +1,25 @@
-console.log("The test extensions is up and running");
+var arr = document.getElementsByClassName("s-code-block");
 
-var images = document.getElementsByTagName('img');
+for (var i = 0; i < arr.length; i++) {
+	//Create button variable for insertion
+	var btn = document.createElement("button");
+	btn.classList.add("copy_code_button");
+	btn.appendChild(document.createTextNode("Copy"));
+	arr[i].appendChild(btn);
 
-for (elt of images) {
-	if (typeof browser === "undefined") {
-		var browser = chrome;
-	}
-	elt.src = `${browser.runtime.getURL("pp.jpg")}`
-	elt.alt = 'an alt text';
+	//Style button
+	btn.style.position = "relative";
+
 }
+
+// console.log("The test extensions is up and running");
+
+// var images = document.getElementsByTagName('img');
+
+// for (elt of images) {
+// 	if (typeof browser === "undefined") {
+// 		var browser = chrome;
+// 	}
+// 	elt.src = `${browser.runtime.getURL("pp.jpg")}`
+// 	elt.alt = 'an alt text';
+// }
